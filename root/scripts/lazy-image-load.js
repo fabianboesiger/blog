34d8462e6cb1b879b5fbd4b9492f0eea1e73e2e0
fabianboesiger <boesiger.fabian@outlook.com>
2019-07-21 22:22:32 +0200

    Changes

--- root/scripts/lazy-image-load.js
+++ root/scripts/lazy-image-load.js
@@ -3,10 +3,13 @@ window.addEventListener("load", () => {
     let images = document.getElementsByTagName("img");
     for(let i = 0; i < images.length; i++) {
         let image = images[i];
-        let download = new Image();
-        download.onload = function(){
-            image.src = this.src;   
-        };
-        download.src = image.getAttribute("lazysrc");
+        let lazysrc = image.getAttribute("lazysrc");
+        if(lazysrc !== null && lazysrc !== undefined) {
+            let download = new Image();
+            download.onload = function(){
+                image.src = this.src;   
+            };
+            download.src = lazysrc;
+        }
     }
 });
